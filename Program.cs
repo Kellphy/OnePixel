88b9e0252f80cff77fbbd9b82be58feda0b6e308
Kellphy <virtual_44@yahoo.com>
2022-12-13 20:10:14 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace OnePixel
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Identifier: ");
+            string identifier = Console.ReadLine();
+            identifier = identifier.Replace(' ','-');
+            string full_name = $"{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Hour}-{DateTime.Now.Minute}-{identifier}";
+            string file = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/" + full_name + ".html";
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                sw.Write($"<!doctype html><img src=\"https://www.kellphy.com/assets/images/opet.png?{full_name}\" alt=\"img\" />");
+            }
+            Console.WriteLine("Completed!");
+            Console.ReadLine();
+            Main();
+        }
+    }
+}
